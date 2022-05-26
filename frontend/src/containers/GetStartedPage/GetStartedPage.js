fe3e6971cdffb86d9267b93e26cff4dc597e176c
amanc1248 <amanc1248@gmail.com>
2022-05-26 14:25:56 +0545

    MADE: responsive page

--- frontend/src/containers/GetStartedPage/GetStartedPage.js
+++ frontend/src/containers/GetStartedPage/GetStartedPage.js
@@ -11,19 +11,19 @@ const GetStartedPage = () => {
 
       {/* get started section */}
       <div className="row get__started__section ">
-        <div className="col text__section">
+        <div className="col col-lg-6 col-md-12 col-sm-12  text__section">
           <div className="text__section__title">
             Manage your employee & user all in one place.
           </div>
           <div className="text__section__subtitle">
-            Admin can add, edit, delete employees and users all from one place.
-            And employees can send any updates from their dashboard via mail
+            Admin can assigns employees and users. And Employees send any
+            updates via email.🎉
           </div>
           <div className="">
             <button className="get__started__button">Get Started</button>
           </div>
         </div>
-        <div className="col image__section">
+        <div className="col col-lg-6 col-md-12 col-sm-12 order-lg-0 order-first image__section">
           <img
             src="https://res.cloudinary.com/proudposhak-com/image/upload/v1653551987/easypizi/busy-project-manager-overwhelmed-by-work_1_ogglay.png"
             alt="getstarted"
